f8bd4998c9d100d61fa2f2839fcb9914e243a1bd
Danny <live626@gmail.com>
2022-04-12 11:52:44 -0400

    environment folder removed from repository

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  HOST: 'https://${HOST}/consultaPesaje-backend',
-  PATH_ROOT: '/consultaPesaje',
-  REINTENTOS: 3,
-  TOKEN_AUTH_USERNAME: 'consultaPesaje',
-  TOKEN_AUTH_PASSWORD: '${TOKEN_AUTH_PASSWORD}',
-  TOKEN_NAME: 'access_token'
-};
